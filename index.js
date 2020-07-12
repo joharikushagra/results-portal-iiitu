276ee0683cd75e7c27c9cefa6e9a1c1fd105054f
pkpsyder007 <pkpsyder007@gmail.com>
2020-07-12 12:49:45 +0530

    INITIAL COMMIT

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.get('/', (req, res) => {
+  res.status(200).json({
+    error: false,
+    body: {
+      msg: 'Welcome to IIITU Results',
+    },
+  });
+});
+
+// Initialize server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT: ${PORT}`);
+});
